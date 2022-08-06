a814b00cd01620193ab1d6f39caf5a92bc0d33ed
abdusami <3338231042@qq.com>
2022-08-06 18:23:56 +0800

    项目初始结构创建

---
+++ gilimall-member/src/main/java/com/asmi/gulimall/member/GilimallMemberApplication.java
@@ -0,0 +1,13 @@
+package com.asmi.gulimall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GilimallMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GilimallMemberApplication.class, args);
+    }
+
+}
